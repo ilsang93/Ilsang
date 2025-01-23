4f23f05dc2112648671354b8f813c4798e1a5fb9
ilsang <pchm20@gmail.com>
2025-01-23 19:50:49 +0900

    2025-01-23 불필요 파일 제거

--- Witch's Stage/Scripts/Utilities/PositionUtilities.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-public static class PositionUtilities
-{
-    public static void Follow(GameObject follower, GameObject target) {
-        
-    }
-}
